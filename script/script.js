91a80b9a4b4f5beae07ef9d5de5bf3fd772f7997
Drangello <drangello0505@gmail.com>
2025-09-17 19:24:49 +0200

    template function erstellt

--- script/script.js
+++ script/script.js
@@ -1,17 +1,11 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const menuSection = document.querySelector(".menu");
-
-  for (let i = 0; i < burgerDaten.length; i++) {
-    const burger = burgerDaten[i];
-    const div = document.createElement("div");
-
-    if (burger.preis > 6) {
-      div.innerHTML = `<h3>${burger.name} ⭐</h3><p>${burger.beschreibung}</p><strong>${burger.preis} €</strong>`;
-    } else {
-      div.innerHTML = `<h3>${burger.name}</h3><p>${burger.beschreibung}</p><strong>${burger.preis} €</strong>`;
-    }
-
-    menuSection.appendChild(div);
+function renderMenuContener(){
+  const refBurger = document.getElementById('menu');
+  refBurger.innerHTML = ""
+  let burgerItem = ""
+  for (let index = 0; index < burgerData.length; index++) {
+    const element = burgerData[index];
+    burgerItem += burgerDataTemplate
   }
-});
+
+}
